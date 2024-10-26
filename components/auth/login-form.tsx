594ca5cc26020abeba02db02050f73ff8b92f58c
skabeo <spencerokyere95@gmail.com>
2024-10-26 16:05:29 +0000

    Add success and error messages

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -4,6 +4,9 @@ import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { LoginSchema } from "@/schemas";
+import { Input } from "@/components/ui/input";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
 
 import {
   Form,
@@ -15,6 +18,7 @@ import {
 } from "@/components/ui/form";
 
 import { CardWrapper } from "./card-wrapper";
+import { Button } from "../ui/button";
 
 const LoginForm = () => {
   const form = useForm<z.infer<typeof LoginSchema>>({
@@ -24,6 +28,10 @@ const LoginForm = () => {
       password: "",
     },
   })
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log(values)
+  }
   
   return (
     <CardWrapper
@@ -34,14 +42,53 @@ const LoginForm = () => {
     >
       <Form {...form}>
         <form
-          onSubmit={form.handleSubmit(() => { })}
+          onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-6"
         >
           <div className="space-y-4">
-            {/* <FormField
-              
-            /> */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="okyere@spencer.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="******"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
+          <FormError message="" />
+          <FormSuccess message="" />
+          <Button
+            type="submit"
+            className="w-full"
+          >
+            Login
+          </Button>
         </form>
       </Form>
     </CardWrapper>
--- schemas/index.tsx
+++ schemas/index.tsx
@@ -2,6 +2,10 @@ import { CgPassword } from "react-icons/cg";
 import * as z from "zod";
 
 export const LoginSchema = z.object({
-  email: z.string().email(),
-  password: z.string()
+  email: z.string().email({
+    message: "Email is required"
+  }),
+  password: z.string().min(1, {
+    message: "Password is required"
+  })
 })
