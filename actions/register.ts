13a2611849795f6a138488300820475099560f77
skabeo <spencerokyere95@gmail.com>
2024-10-27 11:32:44 +0000

    Create a user

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,10 @@
 "use server"
 
 import * as z from "zod";
-
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const Register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -11,5 +13,22 @@ export const Register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" }
   }
 
-  return { success: "Email sent!"}
+  const { name, email, password } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use"}
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    }
+  })
+
+  return { success: "User created"}
 }
